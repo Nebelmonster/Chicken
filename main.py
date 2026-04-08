14eeb5a830743422517bca1707e24c94e5d98b7e
Nebelmonster <hexmex2005@gmail.com>
2026-04-09 00:03:49 +0200

    fixed level reached message code box

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def on_message(message):
     if (not "lastMsg" in data["counters"][str(message.author.id)]["msgs"]) or (time.time() - data["counters"][str(message.author.id)]["msgs"]["lastMsg"] > 10):
         rndm = random.randint(3,7)
         if data["counters"][str(message.author.id)]["msgs"]["xp"] + rndm >= get_next_level_thresh(message.author.id):
-            await message.reply(f"```\nYou reached level {get_level(message.author.id) + 1}!\n```")
+            await message.reply(f"`\nYou reached level {get_level(message.author.id) + 1}!\n`")
         data["counters"][str(message.author.id)]["msgs"]["xp"] += rndm
         data["counters"][str(message.author.id)]["msgs"]["lastMsg"] = time.time()
     if "chicken" in message.content.lower() or "🐔" in message.content.lower():
