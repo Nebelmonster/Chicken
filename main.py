862133b8c4ea0fef9e4efbb33e95e6a3ebfef7f9
Nebelmonster <hexmex2005@gmail.com>
2026-04-10 22:16:26 +0200

    fixed commands

--- main.py
+++ main.py
@@ -34,8 +34,6 @@ async def my_setup():
     await bot.load_extension("commands.resetCommand")
     await bot.load_extension("commands.sayCommand")
     await bot.load_extension("commands.startCommand")
-    bot.tree.clear_commands(guild=None)
-    await bot.tree.sync()
     await tree.sync(guild=discord.Object(id=1487902534545703072))
     print("Ready")
 
