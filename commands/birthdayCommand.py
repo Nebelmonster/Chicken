0e5e892b37483c30bfaa390453b117d7918f588d
Nebelmonster <hexmex2005@gmail.com>
2026-04-15 14:45:54 +0200

    fixed birthday command

--- commands/birthdayCommand.py
+++ commands/birthdayCommand.py
@@ -44,7 +44,7 @@ class BirthdayCommand(commands.Cog):
         birthdays = get_upcoming_birthdays(5, data)
         embed = discord.Embed(colour=Colour.purple(), title="Upcoming Birthdays")
         for (user_id, day) in birthdays:
-            current_user = self.bot.get_guild(1487902534545703072).get_member(user_id)
+            current_user = self.bot.get_guild(1487902534545703072).get_member(int(user_id))
             ordinal = str(day[2]) + ("th" if 4 <= day[2] % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(day[2] % 10, "th"))
 
             embed.add_field(name="", value=f"```\n{current_user.mention}:\n{calendar.month_name[day[1]]} {ordinal}\n```", inline=False)
