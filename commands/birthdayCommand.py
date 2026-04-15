113eff71c93beee75506ea056c803a763d3c68a9
Nebelmonster <hexmex2005@gmail.com>
2026-04-15 12:58:33 +0200

    fixed birthday command

--- commands/birthdayCommand.py
+++ commands/birthdayCommand.py
@@ -1,10 +1,14 @@
 import json
-import time
+import datetime
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
+def get_upcoming_birthdays(num: int, data):
+    birthdays = data["birthdays"]
+    sorted_leaderboard = sorted(birthdays.items(), key=lambda x: x[1]["chicken"], reverse=True)
+    return sorted_leaderboard[:num]
 
 class BirthdayCommand(commands.Cog):
     def __init__(self, bot):
@@ -16,16 +20,19 @@ class BirthdayCommand(commands.Cog):
         app_commands.Choice(name="Set", value="set"),
         app_commands.Choice(name="List", value="list")
     ])
-    async def birthday_command(self, interaction, action: app_commands.Choice[str], date: str, user: discord.User = None):
+    async def birthday_command(self, interaction, action: app_commands.Choice[str], date: str, user:  int):
         data = self.bot.data
-        member = user or interaction.user
-        member_id = str(member.id)
+        #member = user or interaction.user
+        #member_id = str(member.id)
+        member_id = user
         await interaction.response.defer(ephemeral=True)
         if action.value == "set":
-            data["birthdays"][member_id] = time.strptime(date, "%d.%m.%Y")
-            with open("database.json", "w") as filee:
-                json.dump(data, filee, indent=4)
+            date_split = date.split(".")
+            data["birthdays"][member_id] = datetime.datetime(int(date_split[2]), int(date_split[1]), int(date_split[0]))
             await interaction.followup.send("Birthday set.", ephemeral=True)
+        with open("database.json", "w") as filee:
+            json.dump(data, filee, indent=4)
+
 
 
 
