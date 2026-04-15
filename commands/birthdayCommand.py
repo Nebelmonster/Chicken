385ce37dab43e43e14d54325800e1fd1ee76746f
Nebelmonster <hexmex2005@gmail.com>
2026-04-15 14:15:14 +0200

    fixed birthday command

--- commands/birthdayCommand.py
+++ commands/birthdayCommand.py
@@ -21,35 +21,32 @@ class BirthdayCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="birthday", description="Birthday")
-    @app_commands.guilds(discord.Object(id=1487902534545703072))
-    @app_commands.choices(action=[
-        app_commands.Choice(name="Set", value="set"),
-        app_commands.Choice(name="List", value="list")
-    ])
-    async def birthday_command(self, interaction, action: app_commands.Choice[str], date: str, user: discord.User = None):
+    birthday_group = app_commands.Group(name="birthday", description="Manage birthdays")
+
+    @birthday_group.command(name="set", description="Set a users birthday")
+    async def birthday_set(self, interaction, date: str, user: discord.User = None):
         data = self.bot.data
         member = user or interaction.user
         member_id = str(member.id)
         await interaction.response.defer()
-        if action.value == "set":
-            date_split = date.split(".")
-            data["birthdays"][member_id] = [int(date_split[2]), int(date_split[1]), int(date_split[0])]
-            await interaction.followup.send("Birthday set")
-        elif action.value == "list":
-            birthdays = get_upcoming_birthdays(5, data)
-            embed = discord.Embed(colour=Colour.purple(), title="Upcoming Birthdays")
-            for (user_id, day) in birthdays:
-                current_user = self.bot.get_user(int(user_id)).global_name
-                ordinal = str(day[2]) + ("th" if 4 <= day[2] % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(day[2] % 10, "th"))
-
-                embed.add_field(name=f"{current_user}", value=f"```\n{calendar.month_name[day[1]]} {ordinal}\n```", inline=False)
-            await interaction.followup.send(embed=embed)
+        date_split = date.split(".")
+        data["birthdays"][member_id] = [int(date_split[2]), int(date_split[1]), int(date_split[0])]
         with open("database.json", "w") as filee:
             json.dump(data, filee, indent=4)
+        await interaction.followup.send("Birthday set")
 
+    @birthday_group.command(name="list", description="See upcoming birthdays")
+    async def birthday_command(self, interaction):
+        data = self.bot.data
+        await interaction.response.defer()
+        birthdays = get_upcoming_birthdays(5, data)
+        embed = discord.Embed(colour=Colour.purple(), title="Upcoming Birthdays")
+        for (user_id, day) in birthdays:
+            current_user = self.bot.get_user(int(user_id)).global_name
+            ordinal = str(day[2]) + ("th" if 4 <= day[2] % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(day[2] % 10, "th"))
 
-
+            embed.add_field(name=f"{current_user}", value=f"```\n{calendar.month_name[day[1]]} {ordinal}\n```", inline=False)
+        await interaction.followup.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(BirthdayCommand(bot))
