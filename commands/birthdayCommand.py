e26fb472adf91c1ba1164c00e67a01993c2882ab
Nebelmonster <hexmex2005@gmail.com>
2026-04-15 13:33:40 +0200

    fixed birthday command

--- commands/birthdayCommand.py
+++ commands/birthdayCommand.py
@@ -28,19 +28,19 @@ class BirthdayCommand(commands.Cog):
         data = self.bot.data
         member = user or interaction.user
         member_id = str(member.id)
-        await interaction.response.defer(ephemeral=True)
+        await interaction.response.defer()
         if action.value == "set":
             date_split = date.split(".")
             data["birthdays"][member_id] = [int(date_split[2]), int(date_split[1]), int(date_split[0])]
-            await interaction.followup.send("Birthday set.", ephemeral=True)
+            await interaction.followup.send("Birthday set")
         elif action.value == "list":
             birthdays = get_upcoming_birthdays(5, data)
             embed = discord.Embed(colour=Colour.purple(), title="Upcoming Birthdays")
             for (user_id, day) in birthdays:
-                current_user = self.bot.get_user(int(user_id))
+                current_user = self.bot.get_user(int(user_id)).global_name
                 ordinal = str(day[2]) + ("th" if 4 <= day[2] % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(day[2] % 10, "th"))
 
-                embed.add_field(name=f"{current_user.mention}", value=f"```\n{calendar.month_name[day[1]]} {ordinal}\n```", inline=False)
+                embed.add_field(name=f"{current_user}", value=f"```\n{calendar.month_name[day[1]]} {ordinal}\n```", inline=False)
             await interaction.followup.send(embed=embed)
         with open("database.json", "w") as filee:
             json.dump(data, filee, indent=4)
