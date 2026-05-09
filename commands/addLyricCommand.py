0a17a2ea160d0bf8d0a62f6ebca705f3c85279c3
Jason Mueller <jason.mueller@uni-a.de>
2026-05-09 21:50:37 +0200

    added addLyricCommand

--- commands/addLyricCommand.py
+++ commands/addLyricCommand.py
@@ -10,19 +10,19 @@ class AddLyricCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        @app_commands.command(name="addlyric", description="Adds a song to the database")
-        @app_commands.default_permissions(administrator=True)
-        @app_commands.guilds(discord.Object(id=1487902534545703072))
-        async def addlyric_command(self, interaction, song: str, artist: str):
-            await interaction.response.defer()
-            with open("lyrics.json", "r") as file:
-                data = json.load(file)
-            token = "83lKwQdp5pfGc3e2o7PdOFvsAmKngN583fZY3eWtxcEVLH3Rm4eswujKYElmNm8b"
-            genius = lyricsgenius.Genius(token)
-            genius.remove_section_headers = True
-            song = genius.search_song(song, artist)
-            if song:
-                await interaction.followup.send(song.lyrics)
+    @app_commands.command(name="addlyric", description="Adds a song to the database")
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.guilds(discord.Object(id=1487902534545703072))
+    async def addlyric_command(self, interaction, song: str, artist: str):
+        await interaction.response.defer()
+        with open("lyrics.json", "r") as file:
+            data = json.load(file)
+        token = "83lKwQdp5pfGc3e2o7PdOFvsAmKngN583fZY3eWtxcEVLH3Rm4eswujKYElmNm8b"
+        genius = lyricsgenius.Genius(token)
+        genius.remove_section_headers = True
+        song = genius.search_song(song, artist)
+        if song:
+            await interaction.followup.send(song.lyrics)
 
 async def setup(bot):
     await bot.add_cog(AddLyricCommand(bot))
