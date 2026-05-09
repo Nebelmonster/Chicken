01cdcef7410fa11088e69fc31f3ae3a55c291cef
Jason Mueller <jason.mueller@uni-a.de>
2026-05-09 22:29:06 +0200

    fixed addLyricCommand

--- events/lyricReactionSystem.py
+++ events/lyricReactionSystem.py
@@ -19,7 +19,7 @@ class LyricReactionSystem(commands.Cog):
         for song in songs:
             title = song.split(" - ")[0]
             artist = song.split(" - ")[1]
-            if song in message.content.lower():
+            if title in message.content.lower():
                 token = "83lKwQdp5pfGc3e2o7PdOFvsAmKngN583fZY3eWtxcEVLH3Rm4eswujKYElmNm8b"
                 genius = lyricsgenius.Genius(token)
                 genius.remove_section_headers = True
