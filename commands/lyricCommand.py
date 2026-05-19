59799fa66bf3fb25fd570cd7ebc4df7c689adbe9
Jason Mueller <jason.mueller@uni-a.de>
2026-05-19 23:32:28 +0200

    fixed lyricCommand.py

--- commands/lyricCommand.py
+++ commands/lyricCommand.py
@@ -29,13 +29,13 @@ class AddLyricCommand(commands.Cog):
                         with open("database.json", "w") as filee:
                             json.dump(data, filee, indent=4)
                         button.disabled = True
-                        await b_interaction.edit_original_response(f"Added {song.lower()} by {artist.lower()}")
+                        await b_interaction.edit_original_response(content=f"Added {song.lower()} by {artist.lower()}")
                     @discord.ui.button(label="No", style=discord.ButtonStyle.red)
                     async def on_no(self, b_interaction, button):
                         if b_interaction.user.id != interaction.user.id:
                             await b_interaction.response.send_message("You can't interact with this", ephemeral=True)
                             return
-                        await b_interaction.edit_original_response("Song has been discarded")
+                        await b_interaction.edit_original_response(content="Song has been discarded")
 
                 await interaction.followup.send(f"A song with that title already exists in the database:\n`{song.lower()} by {artist_old.lower()}`\nDo you still want to proceed?", view=Confirm())
 
