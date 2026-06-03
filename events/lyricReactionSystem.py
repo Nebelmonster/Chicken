57e6456ecfa186589f4d0307a033da38df576f77
Jason Mueller <jason.mueller@uni-a.de>
2026-06-03 19:12:54 +0200

    adjusted lyricsystem

--- events/lyricReactionSystem.py
+++ events/lyricReactionSystem.py
@@ -1,4 +1,6 @@
 import random
+
+import discord
 import lyricsgenius
 
 from discord.ext import commands
@@ -28,6 +30,7 @@ class LyricReactionSystem(commands.Cog):
             artist = song.split(" - ")[1]
 
             if " " + title + " " in message.content.lower() or message.content.lower().startswith(title + " ") or message.content.lower().endswith(" " + title) or message.content.lower() == title:
+                self.bot.activity = discord.Activity(type=discord.ActivityType.listening, name=title)
                 token = "83lKwQdp5pfGc3e2o7PdOFvsAmKngN583fZY3eWtxcEVLH3Rm4eswujKYElmNm8b"
                 genius = lyricsgenius.Genius(token)
                 genius.remove_section_headers = True
